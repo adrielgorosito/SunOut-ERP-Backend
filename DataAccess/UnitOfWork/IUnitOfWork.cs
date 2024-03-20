789d670c2a2d980f7ebf95be22151f5f33f1b1b2
Adriel Gorosito <adrielgorosito14@gmail.com>
2024-03-20 00:54:26 -0300

    Implemented UnitOfWork pattern

---
+++ DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using SunOut_ERP_Backend.DataAccess.Repositories;
+
+namespace SunOut_ERP_Backend.DataAccess.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        public UsersRepository UserRepository { get; }
+
+        void SaveChangesAsync();
+    }
+}
---
+++ DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using SunOut_ERP_Backend.DataAccess.Repositories;
+
+namespace SunOut_ERP_Backend.DataAccess.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private SunOutDbContext context;
+
+        public UsersRepository UserRepository { get; set; }
+
+        public UnitOfWork(SunOutDbContext context)
+        {
+            this.context = context;
+            this.UserRepository = new UsersRepository(context);
+        }
+
+        public void SaveChangesAsync()
+        {
+            this.context.SaveChangesAsync();
+        }
+    }
+}
