c27b72718460e4921159aace9d5838e95be43bee
Adriel Gorosito <adrielgorosito14@gmail.com>
2024-03-20 23:17:01 -0300

    Added CORS, UOW and more config

--- Program.cs
+++ Program.cs
@@ -1,25 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SunOut_ERP_Backend.DataAccess;
+using SunOut_ERP_Backend.DataAccess.UnitOfWork;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDbContext<SunOutDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SunOutDb")));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt => opt.AddPolicy("AllowOrigin", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors("AllowOrigin");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
